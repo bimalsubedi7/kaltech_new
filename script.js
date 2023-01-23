96a1504fd953be34e71402e3653e1944d5581286
bimalsubedi7 <106547194+bimalsubedi7@users.noreply.github.com>
2023-01-23 23:07:01 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -7,14 +7,21 @@ setInterval(function () {
   }
 }, 5000);
 
-
-let navItem = document.querySelectorAll('.nav-mbl-item')
-
-for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener("click", function () {
-    for (let j = 0; j < navItem.length; j++) {
-      navItem[j].classList.remove("active")
-    }
-    this.classList.add("active")
-  })
+const mblNavList = document.querySelectorAll(".mbl-nav-list");
+function activeLink() {
+  mblNavList.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
 }
+
+mblNavList.forEach((item) => item.addEventListener("click", activeLink));
+
+// let navItem = document.querySelectorAll('.nav-mbl-item')
+
+// for (let i = 0; i < navItem.length; i++) {
+//   navItem[i].addEventListener("click", function () {
+//     for (let j = 0; j < navItem.length; j++) {
+//       navItem[j].classList.remove("active")
+//     }
+//     this.classList.add("active")
+//   })
+// }
