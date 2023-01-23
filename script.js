ad4ad1725d7f08e5f22131634c949e85463f7cd7
bimalsubedi7 <106547194+bimalsubedi7@users.noreply.github.com>
2023-01-23 12:28:05 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ setInterval(function () {
     counter = 1;
   }
 }, 5000);
+
+
+let navItem = document.querySelectorAll('.nav-mbl-item')
+
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("click", function () {
+    for (let j = 0; j < navItem.length; j++) {
+      navItem[j].classList.remove("active")
+    }
+    this.classList.add("active")
+  })
+}
